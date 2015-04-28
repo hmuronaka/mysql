6106850186a66cf349ee4c7bac6a42fa9021d2b5
hmuronaka <capriccio27@gmail.com>
2015-04-28 22:46:02 +0900

    updated

--- libraries/resource_mysql_service.rb
+++ libraries/resource_mysql_service.rb
@@ -8,6 +8,7 @@ class Chef
       default_action :create
 
       attribute :charset, kind_of: String, default: 'utf8'
+      attribute :collation_server, kind_of: String, default: 'utf8'
       attribute :data_dir, kind_of: String, default: nil
       attribute :initial_root_password, kind_of: String, default: 'ilikerandompasswords'
       attribute :instance, kind_of: String, name_attribute: true
